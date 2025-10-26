6ee748cb0a6c6ca5c73906d16ddac3aa38d67dae
AnuragSatbhaiAI <anuragsatbhai@gmail.com>
2025-10-26 21:52:42 +0530

    Add Assignment_1, Assignment_2, Assignmet_3 solutions (Q1 to Q5)

---
+++ Classwork/Day_01/program4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+//static program
+int main()
+{
+    int i = 10, j = 11, ans = 0;
+
+    ans = i + j;
+
+    printf("Addition is : %d\n",ans);
+
+    return 0;
+}
---
+++ Classwork/Day_01/program6.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+//dynamic program
+
+int Add(int a, int b)
+{
+    int sum = 0; 
+    sum = a + b;  //Business logic
+    return sum;
+
+}
+
+
+int main()
+{
+    int i = 0, j = 0, ans = 0;
+
+    printf("Enter first number : \n");
+    scanf("%d",&i);
+
+    printf("Enter second number : \n");
+    scanf("%d",&j);
+
+    ans = Add(i,j);
+
+    printf("Addition is : %d\n",ans);
+
+    return 0;
+}

99717fd59a97fc37f207fbebb6785a29384dbe6b
AnuragSatbhaiAI <anuragsatbhai@gmail.com>
2025-12-17 15:43:56 +0530

    Add Assignment_42 & 43 Solutions (Q1 to Q5)

---
+++ Assignments/Assignment_42/program2.c
@@ -0,0 +1,52 @@
+# include<stdio.h>
+# include<stdlib.h>
+
+# define TRUE 1
+# define FALSE 0
+
+typedef int BOOL;
+
+struct node
+{
+    int Data;
+    struct node *Next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void InsertFirst (PPNODE Head, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> Next = NULL;
+    newn -> Data = no;
+
+    if(*Head == NULL)
+    {
+        *Head = newn;
+    }
+    else
+    {
+        newn -> Next = *Head;
+        *Head = newn;
+    }
+
+}
+
+int main()
+{
+    PNODE First = NULL;
+
+    InsertFirst(&First, 101);
+    InsertFirst(&First, 51);
+    InsertFirst(&First, 21);
+    InsertFirst(&First, 11);
+
+    // Call all functions for below problem statments.
+
+    return 0;
+}
